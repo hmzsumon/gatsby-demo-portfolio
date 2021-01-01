4e170e15a6c6563f3b7c713b0db429fc81815d0f
hmzsumon <zsumonn@gmail.com>
2021-01-01 10:22:40 +0600

    completed gatsby demo gatsby-portfolio website

---
+++ src/components/indexData.js
@@ -0,0 +1,151 @@
+const Person = {
+  name: "H.M. Zakaria Sumon",
+  nationality: "Bangladeshi",
+  languages: "Bengali, English",
+  address: "Dhaka, Bangladesh",
+  freelance: "Available",
+  email: "zaknetsum@gmail.com",
+  phone: "+8801757454532",
+  img: "https://zsumon.netlify.app/src/img/Me.jpg",
+  description:
+    "I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.",
+  socials: [
+    {
+      facebook: "https://www.facebook.com/hussain.m.zakaria",
+      icon: "faFacebookF",
+    },
+    { github: "https://github.com/hmzsumon", icon: "faGithub" },
+    {
+      linkedin: "https://www.linkedin.com/in/zakaria-sumon-7b7b021a9/",
+      icon: "faLinkedinIn",
+    },
+  ],
+  portfolios: [
+    {
+      img: "https://i.ibb.co/PWWDWJ1/Red-Onion.png",
+      link: "https://red-onion-7070.web.app/",
+      git: "https://github.com/hmzsumon/red-onion",
+      title: "Red Onion",
+      description:
+        "A full-stack restaurant app where you can order your favorite food from the menu. You have options for breakfast, lunch, and dinner. Pay online using your credit card and enjoy the most delicious dummy food",
+    },
+    {
+      img: "https://i.ibb.co/rm2s88R/Doctors-Portal.png",
+      link: "https://doctor01.netlify.app/",
+      git: "https://github.com/hmzsumon/doctore-portal",
+      title: "Doctor Portal",
+      description:
+        "A full-stack app where you can pick the date and time and fix an appointment with your doctor. A doctor can review his appointments and prescribe medicine to a patient.",
+    },
+    {
+      img: "https://i.ibb.co/5RJTTsV/Cuda-Protfolio-Template.png",
+      link: "https://cuda-01.netlify.app/",
+      git: "https://github.com/hmzsumon/psd-to-html01",
+      title: "PSD to HTML",
+      description:
+        "This site design for practice. PSD to HTML fully responsive and pixel-perfect design",
+      Technology:
+        " HTML5 , CSS3 , Owl-carousel , Bootstrap , FontAwsome , Jquery",
+    },
+  ],
+  blogs: [
+    {
+      title: "JavaScript Some important topics..",
+      description:
+        "As a JavaScript developer , you should know JavaScript Some important topics you should know as a JavaScript Developer",
+      link:
+        "https://medium.com/@manik89/javascript-some-important-topics-you-should-know-as-a-javascript-developer-21bf65285948",
+    },
+    {
+      title: "JavaScript Some important topics..",
+      description:
+        "As a JavaScript developer , you should know JavaScript Some important topics you should know as a JavaScript Developer",
+      link:
+        "https://medium.com/@manik89/javascript-some-important-topics-you-should-know-as-a-javascript-developer-21bf65285948",
+    },
+    {
+      title: "JavaScript Some important topics..",
+      description:
+        "As a JavaScript developer , you should know JavaScript Some important topics you should know as a JavaScript Developer",
+      link:
+        "https://medium.com/@manik89/javascript-some-important-topics-you-should-know-as-a-javascript-developer-21bf65285948",
+    },
+    {
+      title: "JavaScript Some important topics..",
+      description:
+        "As a JavaScript developer , you should know JavaScript Some important topics you should know as a JavaScript Developer",
+      link:
+        "https://medium.com/@manik89/javascript-some-important-topics-you-should-know-as-a-javascript-developer-21bf65285948",
+    },
+  ],
+  allJods: [
+    {
+      company: "nokshi-bangla",
+      date: "May-2012 - Jun-2013",
+      position: "Manager",
+      desc: [
+        {
+          id: 1,
+          name:
+            "I'm baby woke slow-carb kinfolk godard cred cronut, tilde man bun hashtag subway tile cornhole gentrify artisan pour-over.",
+        },
+        {
+          id: 2,
+          name:
+            "I'm baby woke slow-carb kinfolk godard cred cronut, tilde man bun hashtag subway tile cornhole gentrify artisan pour-over.",
+        },
+        {
+          id: 3,
+          name:
+            "I'm baby woke slow-carb kinfolk godard cred cronut, tilde man bun hashtag subway tile cornhole gentrify artisan pour-over.",
+        },
+      ],
+    },
+    {
+      company: "Push Cyber Cafe",
+      date: "May-2012 - Jun-2013",
+      position: "Supervaisor",
+      desc: [
+        {
+          id: 4,
+          name:
+            "I'm baby woke slow-carb kinfolk godard cred cronut, tilde man bun hashtag subway tile cornhole gentrify artisan pour-over.",
+        },
+        {
+          id: 5,
+          name:
+            "I'm baby woke slow-carb kinfolk godard cred cronut, tilde man bun hashtag subway tile cornhole gentrify artisan pour-over.",
+        },
+        {
+          id: 6,
+          name:
+            "I'm baby woke slow-carb kinfolk godard cred cronut, tilde man bun hashtag subway tile cornhole gentrify artisan pour-over.",
+        },
+      ],
+    },
+    {
+      company: "Magna",
+      date: "May-2012 - Jun-2013",
+      position: "Salse Manager",
+      desc: [
+        {
+          id: 7,
+          name:
+            "I'm baby woke slow-carb kinfolk godard cred cronut, tilde man bun hashtag subway tile cornhole gentrify artisan pour-over.",
+        },
+        {
+          id: 8,
+          name:
+            "I'm baby woke slow-carb kinfolk godard cred cronut, tilde man bun hashtag subway tile cornhole gentrify artisan pour-over.",
+        },
+        {
+          id: 9,
+          name:
+            "I'm baby woke slow-carb kinfolk godard cred cronut, tilde man bun hashtag subway tile cornhole gentrify artisan pour-over.",
+        },
+      ],
+    },
+  ],
+}
+
+export default Person
